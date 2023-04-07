419904deca5a0a449ce1a665e6ea2b07293105a9
roman <konev9494@mail.ru>
2023-04-07 11:12:01 +0300

    HW lesson 2

---
+++ conftest.py
@@ -0,0 +1,21 @@
+import pytest
+from selene.support.shared import browser
+from selene import command
+import random
+import string
+
+
+@pytest.fixture(scope='function')
+def gen_rand_string():
+    letters = string.ascii_lowercase
+    rand_string = ''.join(random.sample(letters, 16))
+    return rand_string
+
+
+@pytest.fixture(scope='session')
+def browser_steps():
+    browser.config.window_width = 1600
+    browser.config.window_height = 900
+    browser.open('https://google.com')
+    yield 'close'
+    browser.close()
---
+++ test_browser_search.py
@@ -0,0 +1,12 @@
+from selene import be, have
+from selene.support.shared import browser
+
+
+def test_google_search(browser_steps):
+    browser.element('[name=q]').should(be.blank).type('yashaka/selene').press_enter()
+    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
+
+def test_negative_google_search(gen_rand_string, browser_steps):
+    browser.element('[name=q]').type(gen_rand_string).press_enter()
+    browser.element('#result-stats').should(have.text('Результатов: примерно 0'))
